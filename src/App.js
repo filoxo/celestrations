d115a677c3cdbae96a12232f89fdb3d36e33596b
Carlos Filoteo <c.filoteo@live.com>
2017-04-28 20:17:33 -0600

    Initialize Firebase with config, add devtools support, include AuthenticationButton component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import * as firebase from "firebase";
+import "firebase/auth";
+import "firebase/database";
+import logo from "./logo.svg";
+import "./App.css";
+import AuthenticationButton from "./auth/AuthenticationButton";
+
+if (module.hot) {
+  require("preact/devtools");
+}
+
+const config = {
+  apiKey: "AIzaSyB46-9EOLw_G68WnpyODa-2d8ShHUEY55w",
+  authDomain: "celestrations.firebaseapp.com",
+  databaseURL: "https://celestrations.firebaseio.com",
+  projectId: "celestrations",
+  storageBucket: "celestrations.appspot.com",
+  messagingSenderId: "427753889270"
+};
+
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
@@ -12,6 +31,7 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <AuthenticationButton />
         </p>
       </div>
     );
