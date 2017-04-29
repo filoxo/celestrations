5cdd4bffdea3cdf64a1a91699c93330807cd9cce
Carlos Filoteo <c.filoteo@live.com>
2017-04-28 22:25:03 -0600

    Add Login component

---
+++ src/Login/Login.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./Login.css";
+
+const Login = () => (
+  <div>
+    <h1>Log in</h1>
+    <input type="text" />
+    <button type="button">Login</button>
+  </div>
+);
+export default Login;
